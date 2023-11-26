aab60a37e15bca3e9944cdcd0282fdfe3af27bed
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2023-11-26 17:55:11 -0300

    feat(functions/bestsellers.ts): adds carousel of items retrieval
    This commit improves the retrieval of cards, now the app returns all info from a given carousel, in
    the future this shall be for all carousel available in the webpage

--- src/functions/bestsellers.ts
+++ src/functions/bestsellers.ts
@@ -1,21 +1,25 @@
-import { extractAmazonCardInformation } from '../libs/extracts';
+import { extractAmazonCarouselInformation } from '../libs/extracts';
 import { accessPage, createBrowser } from '../libs/puppeteer';
 
-import type { ProductInfo } from '../libs/extracts';
+import type { ProductCarouselInfo } from '../libs/extracts';
 
-export async function bestsellers(event): Promise<ProductInfo> {
+export async function bestsellers(): Promise<ProductCarouselInfo> {
 	const browser = await createBrowser();
 	const page = await accessPage(
 		browser,
 		'https://www.amazon.com.br/bestsellers',
 	);
 
-	const productInfo = await page.$eval(
-		'li.a-carousel-card',
-		extractAmazonCardInformation,
+	await page.exposeFunction('extractAmazonCarouselInformation', (carousel) =>
+		extractAmazonCarouselInformation(carousel),
+	);
+
+	const bestSellingCarousel = await page.$eval(
+		'div#zg_left_col1 > div',
+		extractAmazonCarouselInformation,
 	);
 
 	await browser.close();
 
-	return productInfo;
+	return bestSellingCarousel;
 }
--- src/libs/extracts.ts
+++ src/libs/extracts.ts
@@ -4,18 +4,37 @@ export interface ProductInfo {
 	productPrice: number;
 }
 
-export function extractAmazonCardInformation(card: HTMLLIElement): ProductInfo {
-	// console.log(card);
-	const productName: string =
-		card?.querySelector('a > span > div')?.innerHTML ?? 'productName';
-	const productImg: string =
-		card?.querySelector('div > img')?.getAttribute('src') ?? 'productImg';
-	const producPriceExtract: string =
-		card.querySelector('div > span > span')?.innerHTML.trim() ?? '0';
-	const productPrice: number =
-		parseInt(producPriceExtract?.substring(8).replace(',', '')) ?? 0;
-	// const productName = 'productName';
-	// const productImg = 'productImg';
-	// const productPrice = 0;
-	return { productName, productImg, productPrice };
+export interface ProductCarouselInfo {
+	category: string;
+	products: ProductInfo[];
+}
+
+export function extractAmazonCarouselInformation(
+	carousel: HTMLDivElement,
+): ProductCarouselInfo {
+	const extractCard = (card: HTMLLIElement): ProductInfo => {
+		const productName: string =
+			card?.querySelector('a > span > div')?.innerHTML ?? 'productName';
+		const productImg: string =
+			card?.querySelector('div > img')?.getAttribute('src') ?? 'productImg';
+		const producPriceExtract: string =
+			card.querySelector('div > span > span')?.innerHTML.trim() ?? '0';
+		const productPrice: number =
+			parseInt(producPriceExtract?.substring(8).replace(',', '')) ?? 0;
+		return { productName, productImg, productPrice };
+	};
+
+	const category: string =
+		carousel?.querySelector('h2')?.innerText.substring(17) ?? 'category';
+
+	const productsRaw: NodeListOf<HTMLLIElement> =
+		carousel?.querySelectorAll('li.a-carousel-card');
+
+	const products: ProductInfo[] = [];
+
+	productsRaw.forEach((card) => {
+		products.push(extractCard(card));
+	});
+
+	return { category, products };
 }
