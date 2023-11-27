3f4117842e7b8586f15c8d144b4e034e81bea503
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2023-11-27 16:35:46 -0300

    feat(bestsellers.ts): adds dynamodb integration, saving the scraped data

--- src/functions/bestsellers.ts
+++ src/functions/bestsellers.ts
@@ -1,5 +1,7 @@
 import { extractAmazonBestsellingProductsInformation } from '../libs/extracts';
 import { accessPage, createBrowser } from '../libs/puppeteer';
+import { v4 as uuidv4 } from 'uuid';
+import AWS from 'aws-sdk';
 
 import type { BestsellingProductsInfo } from '../libs/extracts';
 
@@ -22,6 +24,22 @@ export async function bestsellers(): Promise<BestsellingProductsInfo> {
 	);
 
 	await browser.close();
+
+	const dynamoDb = new AWS.DynamoDB.DocumentClient();
+	const TABLE_NAME = process.env.DYNAMODB_BESTSELLINGPRODUCTS_TABLE;
+
+	if (TABLE_NAME === null || TABLE_NAME === undefined) {
+		throw new Error('dynamodb table missing');
+	}
+	const putParams = {
+		TableName: TABLE_NAME,
+		Item: {
+			productsId: uuidv4(),
+			BestsellingProducts: bestSellingProductsInfo,
+		},
+	};
+
+	await dynamoDb.put(putParams).promise();
 
 	return bestSellingProductsInfo;
 }
