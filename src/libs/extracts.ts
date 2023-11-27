8f3f836f62fc37c9165f92304a5917d1fdf16b76
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2023-11-27 17:17:45 -0300

    feat(functions/bestsellers.ts): adds endpoint returning only top three items of amazon bestsellers
    Changes the allBestsellers endpoint to /bestsellers/all and adds a endpoint that returns only the
    top three on /bestsellers
    
    BREAKING CHANGE: endpoint /bestsellers now returns another type of data

--- src/libs/extracts.ts
+++ src/libs/extracts.ts
@@ -15,36 +15,6 @@ export interface BestsellingProductsInfo {
 	productsSortedByCategory: ProductCarouselInfo[];
 }
 
-// export function extractAmazonCarouselInformation(
-// 	carousel: HTMLDivElement,
-// ): ProductCarouselInfo {
-// 	const extractCard = (card: HTMLLIElement): ProductInfo => {
-// 		const productName: string =
-// 			card?.querySelector('a > span > div')?.innerHTML ?? 'productName';
-// 		const productImg: string =
-// 			card?.querySelector('div > img')?.getAttribute('src') ?? 'productImg';
-// 		const producPriceExtract: string =
-// 			card.querySelector('div > span > span')?.innerHTML.trim() ?? '0';
-// 		const productPrice: number =
-// 			parseInt(producPriceExtract?.substring(8).replace(',', '')) ?? 0;
-// 		return { productName, productImg, productPrice };
-// 	};
-
-// 	const category: string =
-// 		carousel?.querySelector('h2')?.innerText.substring(17) ?? 'category';
-
-// 	const productsRaw: NodeListOf<HTMLLIElement> =
-// 		carousel?.querySelectorAll('li.a-carousel-card');
-
-// 	const products: ProductInfo[] = [];
-
-// 	productsRaw.forEach((card) => {
-// 		products.push(extractCard(card));
-// 	});
-
-// 	return { category, products };
-// }
-
 export function extractAmazonBestsellingProductsInformation(
 	bestsellingContainer: HTMLDivElement,
 ): BestsellingProductsInfo {
