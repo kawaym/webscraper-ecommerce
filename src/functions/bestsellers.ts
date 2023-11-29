e3539a12f8d49abe92a60018d7032155e693ce6c
Kaway Marinho <57325050+kawaym@users.noreply.github.com>
2023-11-29 11:04:00 -0300

    feat(functions/bestsellers.ts): adds HTTP error handling

--- src/functions/bestsellers.ts
+++ src/functions/bestsellers.ts
@@ -1,62 +1,133 @@
 import { extractBestsellingProducts } from '../libs/extracts';
 import { accessPage, createBrowser } from '../libs/puppeteer';
 
-import type {
-	BestsellingProductsInfo,
-	ProductCarouselInfo,
-	ProductInfo,
-} from '../libs/extracts';
+import type { ProductCarouselInfo, ProductInfo } from '../libs/extracts';
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 
 import { insertItemIntoDb } from '../libs/dynamo';
 import { chooseService } from '../libs/utils';
 
+interface HTTPResponse {
+	statusCode: 200 | 404 | 400 | 500;
+	body?: string;
+}
+
 export async function allBestsellers(
 	event: APIGatewayProxyEvent,
-): Promise<BestsellingProductsInfo> {
-	const service = chooseService(event.queryStringParameters?.service);
+): Promise<HTTPResponse> {
+	try {
+		const service = chooseService(event.queryStringParameters?.service);
 
-	const browser = await createBrowser();
-	const page = await accessPage(browser, service);
+		const browser = await createBrowser();
+		const page = await accessPage(browser, service);
 
-	const bestSellingProductsInfo = await extractBestsellingProducts(
-		page,
-		service,
-	);
+		const bestSellingProductsInfo = await extractBestsellingProducts(
+			page,
+			service,
+		);
 
-	await browser.close();
+		await browser.close();
 
-	await insertItemIntoDb(bestSellingProductsInfo);
+		await insertItemIntoDb(bestSellingProductsInfo);
 
-	return bestSellingProductsInfo;
+		return { statusCode: 200, body: JSON.stringify(bestSellingProductsInfo) };
+	} catch (error) {
+		let message: string;
+		if (error instanceof Error) {
+			message = error.message;
+			if (message === 'Service not found, please try again') {
+				return {
+					statusCode: 400,
+					body: JSON.stringify({ message }),
+				};
+			} else if (message.startsWith('net::ERR')) {
+				return {
+					statusCode: 404,
+					body: JSON.stringify({
+						message:
+							'Error connecting to the chosen service, please contact support',
+					}),
+				};
+			} else {
+				return {
+					statusCode: 500,
+					body: JSON.stringify({
+						message,
+					}),
+				};
+			}
+		} else {
+			return {
+				statusCode: 500,
+				body: JSON.stringify({
+					message: 'Internal Server Error. Please try again later.',
+				}),
+			};
+		}
+	}
 }
 
 export async function bestsellers(
 	event: APIGatewayProxyEvent,
-): Promise<ProductInfo[]> {
-	const service = chooseService(event.queryStringParameters?.service);
+): Promise<HTTPResponse> {
+	try {
+		const service = chooseService(event.queryStringParameters?.service);
 
-	const browser = await createBrowser();
-	const page = await accessPage(browser, service);
+		const browser = await createBrowser();
+		const page = await accessPage(browser, service);
 
-	const bestSellingProductsInfo = await extractBestsellingProducts(
-		page,
-		service,
-	);
+		const bestSellingProductsInfo = await extractBestsellingProducts(
+			page,
+			service,
+		);
 
-	const bestsellers: ProductInfo[] = [];
+		const bestsellers: ProductInfo[] = [];
 
-	const firstCarousel: ProductCarouselInfo =
-		bestSellingProductsInfo.productsSortedByCategory[0];
+		const firstCarousel: ProductCarouselInfo =
+			bestSellingProductsInfo.productsSortedByCategory[0];
 
-	for (let i = 0; i < 3; i++) {
-		// eslint-disable-next-line security/detect-object-injection
-		bestsellers.push(firstCarousel.products[i]);
+		for (let i = 0; i < 3; i++) {
+			// eslint-disable-next-line security/detect-object-injection
+			bestsellers.push(firstCarousel.products[i]);
+		}
+
+		await browser.close();
+
+		await insertItemIntoDb(bestsellers);
+
+		return { statusCode: 200, body: JSON.stringify(bestsellers) };
+	} catch (error) {
+		let message: string;
+		if (error instanceof Error) {
+			message = error.message;
+			if (message === 'Service not found, please try again') {
+				return {
+					statusCode: 400,
+					body: JSON.stringify({ message }),
+				};
+			} else if (message.startsWith('net::ERR')) {
+				return {
+					statusCode: 404,
+					body: JSON.stringify({
+						message:
+							'Error connecting to the chosen service, please contact support',
+					}),
+				};
+			} else {
+				return {
+					statusCode: 500,
+					body: JSON.stringify({
+						message,
+					}),
+				};
+			}
+		} else {
+			return {
+				statusCode: 500,
+				body: JSON.stringify({
+					message: 'Internal Server Error. Please try again later.',
+				}),
+			};
+		}
 	}
-
-	await browser.close();
-
-	await insertItemIntoDb(bestsellers);
-
-	return bestsellers;
 }
--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -2,5 +2,9 @@ export function chooseService(service: string | undefined | null): string {
 	if (service === 'americanas') {
 		return 'https://www.americanas.com.br/';
 	}
-	return 'https://www.amazon.com.br/bestsellers';
+	if (service === 'amazon' || service === undefined || service === null) {
+		return 'https://www.amazon.com.br/bestsellers';
+	}
+
+	throw new Error('Service not found, please try again');
 }
